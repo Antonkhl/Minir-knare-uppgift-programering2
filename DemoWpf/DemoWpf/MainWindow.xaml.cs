3f40617df6f140cc9af8463bc51e875d18b2fb86
Antonkhl <anton.khlopotin@elev.ga.ntig.se>
2020-11-17 12:42:46 +0100

    lägger till så att det faktiskt adderar, substraherar osv och clear knappen

--- DemoWpf/DemoWpf/MainWindow.xaml.cs
+++ DemoWpf/DemoWpf/MainWindow.xaml.cs
@@ -59,9 +59,93 @@ namespace DemoWpf
 
         }
 
-        
+        private void button_variables_click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button knapp)
+            {
+                switch (knapp.Content)
+                {
+                    case "1":
+                    case "2":
+                    case "3":
+                    case "4":
+                        screen.Text += knapp.Content.ToString();
+
+                        break;
+                    default:
+                        break;
+                }
+                screen.Text += knapp.Content.ToString();
+            }
+        }
+
+        private void button_result(object sender, RoutedEventArgs e)
+        {
+            String divisonDefiniton;
+            int indexNumber = 0;
+
+            if (screen.Text.Contains("+"))
+            {
+                indexNumber = screen.Text.IndexOf("+");
+            }
+
+            if (screen.Text.Contains("-"))
+            {
+                indexNumber = screen.Text.IndexOf("-");
+            }
+
+            if (screen.Text.Contains("-"))
+            {
+                indexNumber = screen.Text.IndexOf("-");
+            }
+
+            if (screen.Text.Contains("*"))
+            {
+                indexNumber = screen.Text.IndexOf("*");
+            }
+
+            if (screen.Text.Contains("/"))
+            {
+                indexNumber = screen.Text.IndexOf("/");
+            }
+
+            divisonDefiniton = screen.Text.Substring(indexNumber, 1);
+            double firstNumber = Convert.ToDouble(screen.Text.Substring(0, indexNumber));
+            double SecondNumber = Convert.ToDouble(screen.Text.Substring(indexNumber + 1, screen.Text.Length - indexNumber - 1));
+
+            if (divisonDefiniton == "+")
+            {
+                screen.Text += "=" + (firstNumber + SecondNumber);
+            }
+
+            if (divisonDefiniton == "-")
+            {
+                screen.Text += "=" + (firstNumber - SecondNumber);
+            }
+
+            if (divisonDefiniton == "*")
+            {
+                screen.Text += "=" + (firstNumber * SecondNumber);
+            }
+
+            if (divisonDefiniton == "/")
+            {
+                screen.Text += "=" + (firstNumber / SecondNumber);
+            }
+
 
         }
+
+        private void button_clearing(object sender, RoutedEventArgs e)
+        {
+            screen.Text = "";
+
+        }
+
+
+
+
+    }
     }
 
     
